b19abcefc95b491ee886307d11d75a9e5bbee73e
Anastasia <shevchenko.nasta13@gmail.com>
2022-02-22 22:21:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+const numberOfFilms = +prompt( "how much films films have you already seen?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ("One of the last movies you watched?", ''),
+      b = +prompt ('How much would you rate it?', ''),
+      c = prompt ("One of the last movies you watched?", ''),
+      d = +prompt ('How much would you rate it?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies [c] = d;
+
+console.log (personalMovieDB);      
